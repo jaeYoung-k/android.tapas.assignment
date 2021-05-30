05e7331b21ac7ac763d74acacf0327457f24ba3d
kimjeayoung <a897541@gmail.com>
2021-05-31 00:21:14 +0900

    상태바 높이 갚 구하는 Util 추가

--- app/src/main/java/com/jaeyoung/tapasassignment/utilities/Utilities.kt
+++ app/src/main/java/com/jaeyoung/tapasassignment/utilities/Utilities.kt
@@ -11,3 +11,10 @@ fun View.onThrottleClick(action: (v: View) -> Unit) {
     val listener = View.OnClickListener { action(it) }
     setOnClickListener(OnThrottleClickListener(listener))
 }
+
+fun Context.getStatusBarHeight(): Int {
+    val id = resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (id <= 0) return 0
+
+    return resources.getDimensionPixelSize(id)
+}
