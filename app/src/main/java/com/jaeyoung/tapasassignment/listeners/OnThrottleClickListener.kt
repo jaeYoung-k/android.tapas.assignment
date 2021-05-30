9d882d8dffa9d49b598a7471e82eaa750c8da254
kimjeayoung <a897541@gmail.com>
2021-05-31 00:00:31 +0900

    중복 클릭 방지 listener 추가

---
+++ app/src/main/java/com/jaeyoung/tapasassignment/listeners/OnThrottleClickListener.kt
@@ -0,0 +1,24 @@
+package com.jaeyoung.tapasassignment.listeners
+
+import android.view.View
+
+class OnThrottleClickListener(
+    private val clickListener: View.OnClickListener,
+    private val interval: Long = 300
+) :
+    View.OnClickListener {
+
+    private var clickable = true
+
+    override fun onClick(v: View?) {
+        if (clickable) {
+            clickable = false
+            v?.run {
+                postDelayed({
+                    clickable = true
+                }, interval)
+                clickListener.onClick(v)
+            }
+        }
+    }
+}
